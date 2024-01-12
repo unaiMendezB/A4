f040b946f01975a7cc3b8cd2b574a33f34836f87
unaimendez <unai.mendez@estudiants.urv.cat>
2024-01-12 13:12:08 +0100

    Update a4 optimization.py

--- a4 optimization.py
+++ a4 optimization.py
@@ -1,24 +1,38 @@
 import tsplib95
 import random
 
+
+def fitness(route):
+    return -sum(graph[i][j]['weight'] for i, j in zip(route, route[1:] + route[:1]))
+
+
+def selection_parent(population):
+    # Tournament selection
+    tournament_size = 3
+    return max(random.sample(population, tournament_size), key=fitness)
+
+
+# Load data from .tsp file
 problem = tsplib95.load('fri26.tsp')
+graph = problem.get_graph()
+num_cities = len(graph.nodes)
 
-# Get the list of nodes (cities)
-nodes = list(problem.get_nodes())
-
-# Calculate the distance matrix
-distance_matrix = {}
-for node1 in nodes:
-    distance_matrix[node1] = {}
-    for node2 in nodes:
-        distance_matrix[node1][node2] = problem.get_weight(node1, node2)
-
-num_cities = len(distance_matrix)
-
+# Parameters
+num_generations = 100
 population_size = 50
+crossover_rate = 0.8
+mutation_rate = 0.2
 
+# Create the initial population
 population = []
 for _ in range(population_size):
     individual = list(range(1, num_cities + 1))
     random.shuffle(individual)
     population.append(individual)
+
+# Main Generic_algorithm loop
+for generation in range(num_generations):
+    new_population = []
+    for _ in range(population_size):
+        parent1 = selection_parent(population)
+        parent2 = selection_parent(population)
